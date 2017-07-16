a038ce4d430a5831644dc7ad2538f18724c7ae91
Soulvice <callum@pidg.co>
2017-07-16 11:03:43 +1000

    Error Stacktracing

--- src/lib/utils.es6.js
+++ src/lib/utils.es6.js
@@ -394,6 +394,28 @@ external.fixedTimeComparisonBuffer = (a, b) => {
 }
 
 /*
+  stack trace
+*/
+external.stackTrace = (belowFn) => {
+  var oldLimit = Error.stackTraceLimit;
+  Error.stackTraceLimit = Infinity;
+
+  var dummyObject = {};
+
+  var v8Handler = Error.prepareStackTrace;
+  Error.prepareStackTrace = function(dummyObject, v8StackTrace) {
+    return v8StackTrace;
+  };
+  Error.captureStackTrace(dummyObject, belowFn || external.stackTrace);
+
+  var v8StackTrace = dummyObject.stack;
+  Error.prepareStackTrace = v8Handler;
+  Error.stackTraceLimit = oldLimit;
+
+  return v8StackTrace;
+}
+
+/*
   dirty non-ES6-hack for
   export <item>
 */
