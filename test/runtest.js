94beed1b82c76f46217b084d0d668f14ab876180
s0ul <s0ul@w0lf.io>
2020-01-28 19:44:46 +1000

    Major update and fixes for required audit modules

--- test/runtest.js
+++ test/runtest.js
@@ -1,18 +1,15 @@
-require('babel-polyfill');
+require('@babel/polyfill');
 
-require('babel-register')({
-  ignore: false,
-  only: /.+(?:(?:\.es6\.js)|(?:.jsx))$/,
+require('@babel/register')({
+  ignore: undefined,
+  only: [/.+(?:(?:\.es6\.js)|(?:.jsx))$/],
   extensions: ['.js', '.es6.js', '.jsx'],
   sourceMap: true,
   presets: [
-    'es2015',
-    'stage-3',
+    "@babel/preset-react",
+    "@babel/preset-env",
   ],
   plugins: [
-     'transform-object-rest-spread',
-     'transform-async-to-generator',
-     'transform-class-properties',
-     'syntax-trailing-function-commas',
+     '@babel/plugin-proposal-async-generator-functions',
   ],
 });
